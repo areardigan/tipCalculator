fe336fc7a9418f20e70f4c4c2c266f2c993f8810
andrewreardigan <drewreardigan@gmail.com>
2022-08-28 22:28:02 -0400

    Initial Commit

---
+++ tipCalculator/tipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  tipCalculatorApp.swift
+//  tipCalculator
+//
+//  Created by Andrew Reardigan on 8/28/22.
+//
+
+import SwiftUI
+
+@main
+struct tipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
